aeb6ea7f2fa360a21cab3a7ce37a21df0326a8a3
fletcher burton <burton.fletcher@aol.com>
2023-09-22 15:07:18 -0500

    login and Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import Login from "./components/Login";
+import { Routes, Route, } from "react-router-dom";
+import Register from "./components/Register"
+import NavBar from "./components/NavBar";
+
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState("");
+ 
 
+  // This functions keeps the user logged so they can move from page to page without being logged out.
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(BASE_URL);
+        const result = await response.json();
+        console.log(result);
+
+        setItems(userData.data.posts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+    <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
+    <Routes>
+   ?
+      <Route
+        path="/login"
+        element={
+          <Login
+            setLoggedInUser={setLoggedInUser}
+            setIsLoggedIn={setIsLoggedIn}
+          />
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <Register
+            setLoggedInUser={setLoggedInUser}
+            setIsLoggedIn={setIsLoggedIn}
+          />
+        }
+      />
+      {/* <Route
+        path="/create-post"
+        element={<Create isLoggedIn={isLoggedIn} items={items} setItems={setItems} />}
+      /> */}
+    </Routes>
+  </div>
+);
+}
+  
+  
 export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {UseState} from "react"
+import { useNavigate } from "react-router-dom";
+import "./Login.css"
+
+const COHORT_NAME = "2306-FTB-ET-WEB-AM";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+
+function Login(props) {
+    const[username, setUserName] = useSate("");
+    const[password, setPassword] = useState("");
+    const navigate = useNavigate()
+
+    const handleSumbit = async (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        try {
+            const userData = await loginUser();
+            console.log(userData.data);
+
+            localStorage.setItem("token", userData.data.token);
+            props.setIsLoggedIn(true);
+            props.setIsLoggedInUser(username);
+            
+            navigate('/')
+       }catch(error){
+     console.log(error)
+       }
+    }
+
+    async function loginUser(){
+        try{
+            const response = await fetch(`${BASE_URL}/users/login`, {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                user:{
+                    username: username,
+                    password: password,
+                },
+            })
+            });
+            const userData = await response.json();
+            return userData;
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    return(
+        <div id="login-container">
+            <h1 id="loginheader">login</h1>
+            <form id="loginform" onSubmit={handleSumbit}>
+                <label className="labels">
+                    Username:
+                    <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => {
+                       console.log(e.target.value)
+                       setPassword(e.target.value)
+                    }}
+                    />
+                </label>
+                <label className="labels">
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+
+                <button id="loginbutton" type="sumbit">submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
