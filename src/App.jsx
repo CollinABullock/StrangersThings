6d1b40d8691a5199507eb31dde168085bec0d777
“CollinABullock” <cbullock@schoolofrock.com>
2023-09-26 10:18:21 -0500

    trying to comment out the conflicting files, see if we can merge now

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,108 @@
 
-import { useState, useEffect } from 'react'
-import Login from "./components/Login";
-import { Routes, Route, BrowserRouter, } from "react-router-dom";
-import Register from "./components/Register"
-import NavBar from "./components/NavBar";
-import { useNavigate } from "react-router-dom"
-import AllPosts from './components/AllPosts';
-import CreateNewPost from './components/newPostForm';
-import SinglePost from './components/SinglePost';
-import './App.css'
+// import { useState, useEffect } from 'react'
+// import Login from "./components/Login";
+// import { Routes, Route, BrowserRouter, } from "react-router-dom";
+// import Register from "./components/Register"
+// import NavBar from "./components/NavBar";
+// import { useNavigate } from "react-router-dom"
+// import AllPosts from './components/AllPosts';
+// import CreateNewPost from './components/newPostForm';
+// import SinglePost from './components/SinglePost';
+// import './App.css'
 
-function App() {
+// function App() {
  
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [loggedInUser, setLoggedInUser] = useState("");
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [loggedInUser, setLoggedInUser] = useState("");
  
 
-  // This functions keeps the user logged so they can move from page to page without being logged out.
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(true);
-    }
-  }, []);
+//   // This functions keeps the user logged so they can move from page to page without being logged out.
+//   useEffect(() => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       setIsLoggedIn(true);
+//     }
+//   }, []);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(BASE_URL);
-        const result = await response.json();
-        console.log(result);
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const response = await fetch(BASE_URL);
+//         const result = await response.json();
+//         console.log(result);
 
-        setItems(userData.data.posts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchData();
-  }, []);
-  return (
-    <div>
-    <div className='NavBar'>
-    <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-    </div>
+//         setItems(userData.data.posts);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+//     fetchData();
+//   }, []);
+//   return (
+//     <div>
+//     <div className='NavBar'>
+//     <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+//     </div>
 
-    <Routes>
-      <Route
-        path="/login"
-        element={
-          <Login
-            setLoggedInUser={setLoggedInUser}
-            setIsLoggedIn={setIsLoggedIn}
-          />
-        }
-      />
+//     <Routes>
+//       <Route
+//         path="/login"
+//         element={
+//           <Login
+//             setLoggedInUser={setLoggedInUser}
+//             setIsLoggedIn={setIsLoggedIn}
+//           />
+//         }
+//       />
 
-<Route 
-path="/:id" 
-element={<SinglePost />
-} />
+// <Route 
+// path="/:id" 
+// element={<SinglePost />
+// } />
 
-      <Route
-        path="/register"
-        element={
-          <Register
-            setLoggedInUser={setLoggedInUser}
-            setIsLoggedIn={setIsLoggedIn}
-          />
-        }
-      />
+//       <Route
+//         path="/register"
+//         element={
+//           <Register
+//             setLoggedInUser={setLoggedInUser}
+//             setIsLoggedIn={setIsLoggedIn}
+//           />
+//         }
+//       />
 
-      <Route
-      path='/AllPosts'
-      element={
-        <AllPosts
-        setLoggedInUser={setLoggedInUser}
-        setIsLoggedIn={setIsLoggedIn}
-        />
-      }
-      />
+//       <Route
+//       path='/AllPosts'
+//       element={
+//         <AllPosts
+//         setLoggedInUser={setLoggedInUser}
+//         setIsLoggedIn={setIsLoggedIn}
+//         />
+//       }
+//       />
 
-      <Route
-      path="/CreateNewPost"
-      element={
-        <CreateNewPost
-        setLoggedInUser={setLoggedInUser}
-        setIsLoggedIn={setIsLoggedIn}
-        />
-      }
-      />
+//       <Route
+//       path="/CreateNewPost"
+//       element={
+//         <CreateNewPost
+//         setLoggedInUser={setLoggedInUser}
+//         setIsLoggedIn={setIsLoggedIn}
+//         />
+//       }
+//       />
 
-      {/* <Route
-        path="/create-post"
-        element={<Create isLoggedIn={isLoggedIn} items={items} setItems={setItems} />}
-      /> */}
+//       {/* <Route
+//         path="/create-post"
+//         element={<Create isLoggedIn={isLoggedIn} items={items} setItems={setItems} />}
+//       /> */}
 
 
-    </Routes>
+//     </Routes>
 
-  </div>
-);
-}
+//   </div>
+// );
+// }
 
-<img src="/home/collinabullock/Coursework/Block30/StrangersThings/PICS/STRANGERSTHINGS.jpg" alt="STRANGER'S THINGS" />
+// <img src="/home/collinabullock/Coursework/Block30/StrangersThings/PICS/STRANGERSTHINGS.jpg" alt="STRANGER'S THINGS" />
   
   
-export default App
+// export default App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,46 @@
-import { Link, useNavigate } from "react-router-dom";
-import React from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import React from "react";
 
-function NavBar(props) {
-  let navigate = useNavigate();
-  return (
-    <nav>
-      {props.isLoggedIn ? (
-        <>
-          {/* // This link is only showed when user is logged in. */}
-          {/* <Link className="links" to="/">Posts</Link>
-          <Link className="links" to="/profile">My Profile</Link>
-          <Link className="links" to="/create-post">Create Post</Link> */}
-          <button id="logout-button"
-            onClick={() => {
-              props.setIsLoggedIn(false);
-              localStorage.removeItem("token"); //Removes token from local storage when logout is clicked.
-              navigate("/")
-          }}>Logout
-          </button>
-        </>
-      ) : (
-        <>
-          {/* // These links are showed when the user is logged out. */}
-          {/* <Link className="links" to="/">
-            Posts
-          </Link> */}
-          <Link className="links" to="/login">
-            Login
-          </Link>
-          <Link className="links" to="/register">
-            Register
-          </Link>
-          <Link className="posts" to ="/AllPosts">
-         What's for sale?
-          </Link>
-          <Link className="createpost" to ="/CreateNewPost">
-           Sell Something!
-          </Link>
-        </>
-      )}
-    </nav>
-  );
-}
+// function NavBar(props) {
+//   let navigate = useNavigate();
+//   return (
+//     <nav>
+//       {props.isLoggedIn ? (
+//         <>
+//           {/* // This link is only showed when user is logged in. */}
+//           {/* <Link className="links" to="/">Posts</Link>
+//           <Link className="links" to="/profile">My Profile</Link>
+//           <Link className="links" to="/create-post">Create Post</Link> */}
+//           <button id="logout-button"
+//             onClick={() => {
+//               props.setIsLoggedIn(false);
+//               localStorage.removeItem("token"); //Removes token from local storage when logout is clicked.
+//               navigate("/")
+//           }}>Logout
+//           </button>
+//         </>
+//       ) : (
+//         <>
+//           {/* // These links are showed when the user is logged out. */}
+//           {/* <Link className="links" to="/">
+//             Posts
+//           </Link> */}
+//           <Link className="links" to="/login">
+//             Login
+//           </Link>
+//           <Link className="links" to="/register">
+//             Register
+//           </Link>
+//           <Link className="posts" to ="/AllPosts">
+//          What's for sale?
+//           </Link>
+//           <Link className="createpost" to ="/CreateNewPost">
+//            Sell Something!
+//           </Link>
+//         </>
+//       )}
+//     </nav>
+//   );
+// }
 
-export default NavBar;
+// export default NavBar;
