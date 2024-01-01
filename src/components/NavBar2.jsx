d3a1f85da983bad726c7180d6f6ad520381dfb37
“CollinABullock” <cbullock@schoolofrock.com>
2024-01-01 17:30:11 -0600

    continuing to build out NavBar2

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -3,13 +3,44 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
-export default function NavBar2() {
+export default function MenuAppBar(props) {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isLoggedIn}
+              onChange={() => {
+                props.setIsLoggedIn(false);
+                localStorage.removeItem("token"); //Removes token from local storage when logout is clicked.
+            }}
+              aria-label="login switch"
+            />
+          }
+          label={isLoggedIn ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -22,9 +53,40 @@ export default function NavBar2() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Strangers Things
+            Stranger's Things
           </Typography>
-          <Button color="inherit">Login</Button>
+          {isLoggedIn && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
