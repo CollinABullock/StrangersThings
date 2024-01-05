adb4ca2eee88a5798a265dbac2b50cce8de8e82f
“CollinABullock” <cbullock@schoolofrock.com>
2024-01-05 13:58:37 -0600

    that made things worse

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,45 @@ import Login from "./components/Login";
 import Profile from "./components/Profile";
 import CreatePost from "./components/Create";
 import ResponsiveAppBar from "./components/NavBar2";
-import SignUp from "./components/Register2";
 
 
 
-function App() { 
+
+const COHORT_NAME = "2306-FTB-ET-WEB-AM";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}/posts`;
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState("");
+ 
+
+  // This functions keeps the user logged so they can move from page to page without being logged out.
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(BASE_URL);
+        const result = await response.json();
+        console.log(result);
+
+        setItems(result.data.posts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div id="root">
-      <ResponsiveAppBar />
+      <ResponsiveAppBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
       <Routes>
         <Route path="/profile" element={<Profile />} />
@@ -34,21 +65,21 @@ function App() {
 
         <Route
           path="/post/:id"
-          element={<SingleItem />}
+          element={<SingleItem items={items} isLoggedIn={isLoggedIn} />}
         />
-
-
 
         
           <Route
           path="/navbar2"
-          element={<ResponsiveAppBar />}
+          element={<ResponsiveAppBar items={items} isLoggedIn={isLoggedIn} />}
         />
 
         <Route
           path="/login"
           element={
             <Login
+              setLoggedInUser={setLoggedInUser}
+              setIsLoggedIn={setIsLoggedIn}
             />
           }
         />
@@ -59,18 +90,22 @@ function App() {
           path="/register"
           element={
             <Register
+              setLoggedInUser={setLoggedInUser}
+              setIsLoggedIn={setIsLoggedIn}
             />
           }
         />
         <Route
           path="/createpost"
           element={<CreatePost 
+            setLoggedInUser={setLoggedInUser}
+            setIsLoggedIn={setIsLoggedIn}
           />}
         />
 
         <Route
           path="/allposts"
-          element={<AllItems />}
+          element={<AllItems isLoggedIn={isLoggedIn} items={items} setItems={setItems} />}
         />
 
       </Routes>
