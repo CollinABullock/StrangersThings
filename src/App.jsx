f5308ef54341f226b8014685005c9ca4d4886925
“CollinABullock” <cbullock@schoolofrock.com>
2024-01-05 13:48:37 -0600

    troubleshooting netlify routing problems

--- src/App.jsx
+++ src/App.jsx
@@ -11,45 +11,14 @@ import CreatePost from "./components/Create";
 import ResponsiveAppBar from "./components/NavBar2";
 import SignUp from "./components/Register2";
 
-
-
-
-const COHORT_NAME = "2306-FTB-ET-WEB-AM";
-const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}/posts`;
-
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [loggedInUser, setLoggedInUser] = useState("");
  
-
-  // This functions keeps the user logged so they can move from page to page without being logged out.
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(BASE_URL);
-        const result = await response.json();
-        console.log(result);
-
-        setItems(result.data.posts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchData();
-  }, []);
 
   return (
     <div id="root">
       <ResponsiveAppBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
+<div className="routes">
       <Routes>
         <Route path="/profile" element={<Profile />} />
 
@@ -115,6 +84,7 @@ function App() {
         />
 
       </Routes>
+      </div>
     </div>
   );
 }
